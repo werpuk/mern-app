fe6d67daa067d50ce1028c603c6008ce334633a1
sumanasri <pailasumanasri@gmail.com>
2025-07-28 23:06:56 -0400

    Initial REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express =  require('express');
-const dotenv =  require('dotenv').config();
+const colors = require('colors')
+const dotenv =  require('dotenv').config({ path: './backend/.env' });
+console.log('MONGO_URI:', process.env.MONGO_URI)
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
+
+connectDB()
 
 const app =  express()
 
@@ -9,4 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
